cdd46e34b23d992375eafdedc74265d989b0afd9
NamanMakhija69 <starkastic16@gmail.com>
2021-05-21 22:20:28 +0530

    Fetch The Popcorn

---
+++ src/components/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Ftchthepopcorn.png"
+        alt="Fetch The Popcorn Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/1/15/3eertw.png"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
